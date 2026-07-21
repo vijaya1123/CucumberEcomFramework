e439523d53a0198c937345317b70c4e22b57255b
Tester <64784724+vijaya1123@users.noreply.github.com>
2026-07-21 10:18:37 +0530

    Add tags option to CucumberOptions in TestRunner

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,6 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
+        tags = "${cucumber.filter.tags}",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber.html",
